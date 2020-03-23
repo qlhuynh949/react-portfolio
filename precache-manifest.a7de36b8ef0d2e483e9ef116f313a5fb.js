20eed58ac71d0cfe585db86a0da439a71201a688
Quan Huynh <qlhuynh@gmail.com>
2020-03-23 00:18:43 -0700

    Updates

---
+++ precache-manifest.a7de36b8ef0d2e483e9ef116f313a5fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1464516bb4fbcdb8eeb3870cb2cf12b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3e8343f5190804b2b5d9",
+    "url": "/react-portfolio/static/css/main.fe769e77.chunk.css"
+  },
+  {
+    "revision": "2942cd2c0b7a06c34c2e",
+    "url": "/react-portfolio/static/js/2.c3e80981.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-portfolio/static/js/2.c3e80981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8343f5190804b2b5d9",
+    "url": "/react-portfolio/static/js/main.d56b337d.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  }
+]);
